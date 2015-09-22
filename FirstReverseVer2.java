78a487a4a067d45a5bb074210230ddf7f64d2887
youjin9209 <youjin9200@naver.com>
2015-09-22 18:20:57 +0900

    Create FirstReverseVer2.java

---
+++ FirstReverseVer2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+//Devide and Conquer로 해보기 
+class Function {
+  static String FirstReverse(String str) {
+  
+  
+  return str;
+ 
+}
+ 
+public static void main (String[] args) {
+// keep this function call here
+  Scanner s = new Scanner(System.in);
+  Function c = new Function();
+  System.out.print(c.FirstReverse(s.nextLine()));
+}
+ 
+} 
