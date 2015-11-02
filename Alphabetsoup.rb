be27adc80ccfe787dce9bd11a9e3e8de708b51e6
youjin9209 <youjin9200@naver.com>
2015-11-02 14:11:10 +0900

    Delete Alphabetsoup.rb

--- Alphabetsoup.rb
+++
@@ -1,8 +0,0 @@
-def AlphabetSoup(arg_10)
-  arg_10=arg_10.split('').sort.join('')
-  return arg_10
-end
-   
-# keep this function call here 
-# to see how to enter arguments in Ruby scroll down   
-AlphabetSoup(STDIN.gets)  
