106959e23ac63ad330bbb59b527c2deb7f397f45
youjin9209 <youjin9200@naver.com>
2015-11-10 21:37:35 +0900

    Create VowelCount.java

---
+++ VowelCount.java
@@ -0,0 +1,27 @@
+package coderbyte;
+import java.util.*; 
+import java.io.*;
+
+/* 13. VowelCount
+ *  take the str string parameter being passed and return the number of vowels the string contains (ie. "All cows eat grass" would return 5). 
+ *  Do not count y as a vowel for this challenge.
+ *  ex / Input = "hello" -> Output = 2 / Input = "coderbyte" -> Output = 3
+ */
+public class Function13 {
+	int VowelCount(String str) { 
+		int increments=0;  
+		for (int i = 0; i < str.length(); i++) {
+	        if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' || str.charAt(i)=='o' || str.charAt(i)=='u' )
+	        	increments++;
+	        }
+	    return increments;
+	  }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// keep this function call here     
+	    Scanner  s = new Scanner(System.in);
+	    Function13 c = new Function13();
+	    System.out.print(c.VowelCount(s.nextLine()));
+	}
+
+}
